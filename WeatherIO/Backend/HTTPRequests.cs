42dbdc3d70e7effae4dd67f11860983ca4ee7a07
Tiago Jose Sousa Magalhães <tiagojsmagalhaes@gmail.com>
2019-12-14 15:42:13 +0000

    added methods for executing get requests

---
+++ WeatherIO/Backend/HTTPRequests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherIO.Backend
+{
+    class HTTPRequests
+    {
+        static public string Get(string url,Dictionary<string,string>headers = null)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+            
+            if(headers!=null)
+            {
+                //build request headers
+            }
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+
+            return reader.ReadToEnd();
+        }
+
+        static public Task<string> GetAsync(string url,Dictionary<string,string>headers = null)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+            
+            if(headers!=null)
+            {
+                //build request headers
+            }
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+
+            return reader.ReadToEndAsync();
+        }
+    }
+}
---
+++ WeatherIO/Views/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using SkiaSharp;
+using SkiaSharp.Views.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace WeatherIO
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            SKCanvasView canvasView = new SKCanvasView();
+            canvasView.PaintSurface += OnCanvasViewPaintSurface;
+            Content = canvasView;
+        }
+
+        void OnCanvasViewPaintSurface(object sender, SKPaintSurfaceEventArgs args)
+        {
+            SKImageInfo info = args.Info;
+            SKSurface surface = args.Surface;
+            SKCanvas canvas = surface.Canvas;
+
+            canvas.Clear();
+
+            SKPaint paint = new SKPaint
+            {
+                Style = SKPaintStyle.Stroke,
+                Color = Color.Red.ToSKColor(),
+                StrokeWidth = 25
+            };
+
+            canvas.DrawCircle(info.Width / 2, info.Height / 2, 100, paint);
+
+            paint.Style = SKPaintStyle.Fill;
+            paint.Color = SKColors.Blue;
+            canvas.DrawCircle(args.Info.Width / 2, args.Info.Height / 2, 100, paint);
+        }
+    }
+}
